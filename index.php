093ea3366d1bd0bed1da087b26dbf4e75f551c39
prince116 <princewong116@gmail.com>
2017-08-23 13:21:57 +0800

    DB testing

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-    
-echo '<h1>Hello World</h1>';
-    
+$servername = "localhost";
+$username = "";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
